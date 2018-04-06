5f735dff4e50ef9628f12cabea564161a73bcfab
Timo Lubitz <timo.lubitz@gmail.com>
2018-04-06 21:00:16 +0200

    test user

--- standalone_version/misc.py
+++ standalone_version/misc.py
@@ -14,7 +14,7 @@ def check_delimiter(sbtab_file):
         if row.startswith('!'):
             s = re.search('(.)(!)', row[1:])
             # if there is only 1 column, we have to define a default separator
-            # let's use a tab.
+            # lets use a tab.
             try: sep = s.group(1)
             except: sep = '\t'
 
